a6f4860192f1d368d99a59c0c9be5a76573b52a8
arteeguz <118378371+arteeguz@users.noreply.github.com>
2024-02-06 17:35:30 -0500

    Update CSC382Project1.cpp

--- Project1/CSC382Project1.cpp
+++ Project1/CSC382Project1.cpp
@@ -6,26 +6,37 @@
  */
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <ctime> 
 
 using namespace std;
 
 int FiboR(int n);
 int FiboNR(int n);
 int MODFibR(int n);
-int MODFibR_Helper(int n, vector<int>& memo);
-double measureTime(int (*func)(int), int n);\
 
 int main() {
     cout << "Fibonacci time analysis (recursive vs. non-recursive)\n";
-    cout << "Integer\t\tFiboR(sec)\tMODFibR\t\tFiboNR(sec)\tFibo-value\n";
+    cout << "Integer\t\tFiboR(sec)\tMODFibR(sec)\tFiboNR(sec)\tFibo-value\n";
 
     vector<int> testValues = { 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60 };
     for (int n : testValues) {
-        double timeFiboR = measureTime(FiboR, n);
-        double timeMODFibR = measureTime(MODFibR, n);
-        double timeFiboNR = measureTime(FiboNR, n);
-        int fiboValue = FiboNR(n); // Using FiboNR to get the Fibonacci number (efficient for larger n)
+        clock_t start, end;
+        double timeFiboR, timeMODFibR, timeFiboNR;
+
+        start = clock();
+        FiboR(n);
+        end = clock();
+        timeFiboR = double(end - start) / CLOCKS_PER_SEC;
+
+        start = clock();
+        int fiboValue = MODFibR(n); 
+        end = clock();
+        timeMODFibR = double(end - start) / CLOCKS_PER_SEC;
+
+        start = clock();
+        FiboNR(n);
+        end = clock();
+        timeFiboNR = double(end - start) / CLOCKS_PER_SEC;
 
         cout << n << "\t\t"
             << timeFiboR << "\t\t"
@@ -33,9 +44,11 @@ int main() {
             << timeFiboNR << "\t\t"
             << fiboValue << "\n";
     }
+
     return 0;
 }
 
+// Recursive Fibonacci function
 int FiboR(int n) {
     if (n == 0 || n == 1)
         return n;
@@ -43,9 +56,10 @@ int FiboR(int n) {
         return FiboR(n - 1) + FiboR(n - 2);
 }
 
+// Non-Recursive Fibonacci function
 int FiboNR(int n) {
-    if (n == 0) return 0;
-    vector<int> F(n + 1);
+    const int max = 100;
+    int F[max];
     F[0] = 0; F[1] = 1;
     for (int i = 2; i <= n; i++) {
         F[i] = F[i - 1] + F[i - 2];
@@ -53,25 +67,13 @@ int FiboNR(int n) {
     return F[n];
 }
 
+// Memoized Recursive Fibonacci function
 int MODFibR(int n) {
-    vector<int> memo(n + 1, -1);
-    return MODFibR_Helper(n, memo);
-}
-
-int MODFibR_Helper(int n, vector<int>& memo) {
+    static vector<int> memo(100, -1); 
     if (n == 0 || n == 1)
         return n;
     if (memo[n] != -1)
         return memo[n];
-    else {
-        memo[n] = MODFibR_Helper(n - 1, memo) + MODFibR_Helper(n - 2, memo);
-        return memo[n];
-    }
-}
-
-double measureTime(int (*func)(int), int n) {
-    clock_t start = clock();
-    func(n);
-    clock_t end = clock();
-    return double(end - start) / CLOCKS_PER_SEC;
+    memo[n] = MODFibR(n - 1) + MODFibR(n - 2);
+    return memo[n];
 }
